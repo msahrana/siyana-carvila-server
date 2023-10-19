664845fff86266e3228bb1e67d962cdf78deaccb
msahrana <msahrana@gmail.com>
2023-10-20 05:28:19 +0600

    get section craeted

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crgl3kb.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const carCollection = client.db('carDB').collection('honda')
+
+    app.get('/car', async(req, res)=>{
+      const cursor = carCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/car/:brand/:id', async(req, res)=> {
+      const id = req.params.id 
+      const query = {_id : new ObjectId(id)}
+      const result = await carCollection.findOne(query)
+      res.send(result)
+    })
+    
+    app.get('/car/:id', async(req, res)=> {
+      const id = req.params.id 
+      const query = {_id : new ObjectId(id)}
+      const result = await carCollection.find(query)
+      res.send(result)
+    })
+
+    app.post('/car', async(req, res)=>{
+        const newCar = req.body
+        console.log(newCar)
+        const result =  await carCollection.insertOne(newCar)
+        res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res)=>{
